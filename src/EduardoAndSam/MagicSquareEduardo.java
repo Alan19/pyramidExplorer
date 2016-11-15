d43661e2d36ef9f42691b70e3cec27c6951f1e96
Student8 <Student8@10.8.33.156>
2016-11-15 12:52:13 -0500

    stuff

---
+++ src/EduardoAndSam/MagicSquareEduardo.java
@@ -0,0 +1,12 @@
+package EduardoAndSam;
+
+public class MagicSquareEduardo {
+
+	boolean[][] unchangeables = new boolean[3][3];
+	int[][] magicSquare = new int[3][3];
+	
+	public MagicSquareEduardo() {
+		generateDistictItems
+	}
+
+}
