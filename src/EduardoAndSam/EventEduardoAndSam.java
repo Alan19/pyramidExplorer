6a44a108d65a6c260242f46b1ab64962dca9ba8c
Student8 <Student8@10.8.33.156>
2016-11-21 13:23:50 -0500

    final commit?

--- src/EduardoAndSam/EventEduardoAndSam.java
+++ src/EduardoAndSam/EventEduardoAndSam.java
@@ -13,17 +13,22 @@ public class EventEduardoAndSam extends CaveRoomPd8 implements Playable {
 	//	play();
 	}
 
-	private static final String[] SEQUENCE_1 = {"Suddenly, all the doors slam shut.", "The closest door seems to have some sort of strange mark on it.", "On further inspection, you see that the mark is actually an half-filled grid of numbers", "As you lean in, you realize that there's a faint bit of text underneath the puzzle:", "'To leave this chamber, prove your mastery of the ancient art of magic (squares)!'"}; 
+	private static final String[] SEQUENCE_1 = {"Suddenly, all the doors slam shut.", "The closest door seems to have some sort of strange mark on it.", "On further inspection, you see that the mark is actually an half-filled grid of numbers", "As you lean in, you realize that there's a faint bit of text underneath the puzzle:", "'To leave this chamber, prove your mastery of the ancient art of magic (squares)! [Hint: Always start with a six or a seven next to each other in the top row!]'"}; 
 	private static final String[] SEQUENCE_2 = {"As you scratch the last number into the square, the doors grind open.", "'Well, well, well' you hear, as you let out a sigh of relief.", "This should prove most... interesting", "You continue through the tunnels."};
 	private static Scanner in = new Scanner(System.in);
 	public static int[][] magicSquare;
 	
 	public void play() {
 		readSequence(SEQUENCE_1);
+		magicSquare = MagicSquareEduardo.CreateMagicSquareEduardo();
 		printSquare(magicSquare);
 		while(!SolveMagicSquareSam.check(magicSquare)){
+			if(checkLoss(magicSquare) == false){
+				System.out.println("You seem to be having some issues...");
+			}
 			askForInput();
 			printSquare(magicSquare);
+			
 		}
 		readSequence(SEQUENCE_2);
 	}
@@ -78,6 +83,20 @@ public class EventEduardoAndSam extends CaveRoomPd8 implements Playable {
 		System.out.println("What number do you want to put there?");
 		String input = in.nextLine();
 		SolveMagicSquareSam.interpretInput(input, column, row);
+	}
+	public boolean checkLoss(int[][] magicSquare){
+		int squaresFilled = 0;
+		for(int i = 0; i < magicSquare.length; i++){
+			for(int j = 0; j < magicSquare[i].length; j++){
+				if (magicSquare[i][j] != 0){
+					squaresFilled++;
+				}
+			}
+		}
+		if(squaresFilled == 9){
+			return false;
+		}
+		return true;
 	}
 	
 }
--- src/EduardoAndSam/MagicSquareEduardo.java
+++ src/EduardoAndSam/MagicSquareEduardo.java
@@ -8,24 +8,22 @@ public class MagicSquareEduardo {
 	
 	public static int[][] CreateMagicSquareEduardo() {
 	
-		int[] distinctRow = makeRandomDistictIntList(3, 3);
-		int[] distinctCol = makeRandomDistictIntList(3, 3);
+		int[] distinctRow = generateDistinctItemsList(3, 3);
+		int[] distinctCol = generateDistinctItemsList(3, 3);
 
-		magicSquare[0][0] = (2);
-		magicSquare[0][1] = (7);
-		magicSquare[0][2] = (6);
-		magicSquare[1][0] = (9);
-		magicSquare[1][1] = (5);
-		magicSquare[1][2] = (1);
-		magicSquare[2][0] = (4);
-		magicSquare[2][1] = (3);
-		magicSquare[2][2] = (8);
+		magicSquare[0][0] = 2;
+		magicSquare[0][1] = 7;
+		magicSquare[0][2] = 6;
+		magicSquare[1][0] = 9;
+		magicSquare[1][1] = 5;
+		magicSquare[1][2] = 1;
+		magicSquare[2][0] = 4;
+		magicSquare[2][1] = 3;
+		magicSquare[2][2] = 8;
 		for(int i = 0; i < 3; i++){
 			magicSquareUn[distinctRow[i]-1][distinctCol[i]-1] = magicSquare[distinctRow[i]-1][distinctCol[i]-1];
 			unchangeables[distinctRow[i]-1][distinctCol[i]-1] = true;
 		}
-		int extraRow = (int)(Math.random()*3+1); 
-		int extraCol = (int)(Math.random()*3+1);
 		return magicSquareUn;
 	}
 	
@@ -56,23 +54,4 @@ public class MagicSquareEduardo {
         return itemList; 
     }
 	
-	public static int[] makeRandomDistictIntList(int length, int max){
-		int[] array = new int[length];
-		for(int i = 0; i < array.length; i++){
-			int rand = (int)(Math.random()*max+1);
-			while(itemisNotInList(array,rand)){
-				rand = (int)(Math.random()*max+1);
-			}
-			array[i] = rand;
-		}
-		return array;
-	}
-
-	private static boolean itemisNotInList(int[] array, int rand) {
-		for(int i = 0; i < array.length; i++){
-			if(array[i] == rand)
-				return false;
-		}
-		return true;
-	}
 }
