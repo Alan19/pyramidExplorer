72091e1214d7828e39e35e0e05c4ec912e14a476
maret <maret@LAPTOP-N6IN2UP4>
2016-12-12 18:02:31 -0500

    4x4 msqr part 1 trial 1

--- src/EduardoAndSam/MagicSquareEduardo.java
+++ src/EduardoAndSam/MagicSquareEduardo.java
@@ -1,15 +1,19 @@
 package EduardoAndSam;
 
+import java.util.Arrays;
+
 public class MagicSquareEduardo {
 
-	private static boolean[][] unchangeables = new boolean[3][3];
-	private static int[][] magicSquare = new int[3][3];
-	private static int[][] magicSquareUn = new int[3][3];
+	private static boolean[][] unchangeables = new boolean[4][4];
+	private static int[][] magicSquare = new int[4][4];
+	private static int[][] magicSquareUn = new int[4][4];
+	private static boolean[] 16To1Used = new boolean[16];
+	
 	
 	public static int[][] CreateMagicSquareEduardo() {
 	
-		int[] distinctRow = generateDistinctItemsList(3, 3);
-		int[] distinctCol = generateDistinctItemsList(3, 3);
+		int[] distinctRow = generateDistinctItemsList(4);
+		int[] distinctCol = generateDistinctItemsList(4);
 
 		magicSquare[0][0] = 2;
 		magicSquare[0][1] = 7;
@@ -20,12 +24,31 @@ public class MagicSquareEduardo {
 		magicSquare[2][0] = 4;
 		magicSquare[2][1] = 3;
 		magicSquare[2][2] = 8;
-		for(int i = 0; i < 3; i++){
+		
+//		int numToUse = (int)(16*Math.random()+1);
+//		for(int row = 0; row < magicSquare.length; row++){
+//			for(int col = 0; col < magicSquare[row].length; col++){
+//				int potentialRowSum = arraySum(magicSquare[row])+numToUse;
+//				while(!16To1Used[numToUse-1] || potentialRowSum > 34 || potentialRowSum == 34 && col != magicSquare[row].length-1){
+//					numToUse = (int)(16*Math.random()+1);
+//				}
+//				magicSquare[row][col] == numToUse;
+//			}
+//		}
+		
+		for(int i = 0; i < 4; i++){
 			magicSquareUn[distinctRow[i]-1][distinctCol[i]-1] = magicSquare[distinctRow[i]-1][distinctCol[i]-1];
 			unchangeables[distinctRow[i]-1][distinctCol[i]-1] = true;
 		}
 		return magicSquareUn;
 	}
+	
+//	public static int arraySum(int[] array){
+//		if(array.length = 0){
+//			return array[1];
+//		}
+//		return array[array.length-1]+arraySum(Arrays.copyOfRange(array, 0, array.length-1));
+//	}
 	
 	public static int[][] giveCompleteSquare(){
 		return magicSquare;
@@ -39,12 +62,12 @@ public class MagicSquareEduardo {
 		return unchangeables;
 	}
 	
-	public static int[] generateDistinctItemsList(int n, int m){
-        int[] itemList = new int[n];
-		boolean[] repeatedNumbers = new boolean [m];
-		double varMod = (m);
+	public static int[] generateDistinctItemsList(int arrayLength){
+        int[] itemList = new int[arrayLength];
+		boolean[] repeatedNumbers = new boolean [arrayLength];
+		double varMod = (arrayLength);
+		int rand = (int)(varMod*Math.random()+1); 
 		for(int i = 0; i < itemList.length; i++){
-			int rand = (int)(varMod*Math.random()+1); 
 			while(repeatedNumbers[rand-1] == true){
 				rand = (int)(varMod*Math.random()+1);
 			}
